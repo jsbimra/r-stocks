ccfe3fcfb5ce4aa58afbdf27630338465e774330
jsbimra <jsbimra@users.noreply.github.com>
2019-05-22 00:59:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"r-stock test" 
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+
+import Layout from '../components/Layout';
+
+const About = () => {
+    return (
+        <Layout>
+            About content!
+        </Layout>
+    )
+}
+
+export default About;
